22a2590e00eaa4c296667fc938b8341620b5196e
Simen Norderud jensen <simennj@outlook.com>
2017-10-06 13:47:58 +0200

    add printing of left joystick input

--- controller/controller.ino
+++ controller/controller.ino
@@ -3,5 +3,9 @@ void setup() {
 }
 
 void loop() {
-  
+  int left_horisontal = analogRead(A1);
+  int left_vertical = analogRead(A2);
+  Serial.print(left_horisontal);
+  Serial.print(' ');
+  Serial.println(left_vertical);
 }
